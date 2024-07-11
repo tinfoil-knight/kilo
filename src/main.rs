7123dfe951b6ce9e9d5867bcc1dd898a1ff16cd8
Kunal Kundu <kunal99kundu@gmail.com>
2024-07-12 03:48:47 +0530

    turn off echoing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::{
+    io::{self, Read},
+    mem,
+};
+
+use libc::{tcgetattr, tcsetattr, termios, ECHO, STDIN_FILENO, TCSAFLUSH};
+
+fn enable_raw_mode() {
+    unsafe {
+        let mut raw: termios = mem::zeroed();
+
+        tcgetattr(STDIN_FILENO, &mut raw);
+        raw.c_lflag &= !ECHO;
+        tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    enable_raw_mode();
+
+    let mut c = [0; 1];
+
+    while let Ok(_) = io::stdin().read_exact(&mut c) {
+        if &c == b"q" {
+            break;
+        }
+    }
 }
