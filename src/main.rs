723b459d28e09259b78304a0804f8af7d4450b81
Kunal Kundu <kunal99kundu@gmail.com>
2024-07-12 13:00:49 +0530

    disable raw mode at exit

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,21 @@ use std::{
     mem,
 };
 
-use libc::{tcgetattr, tcsetattr, termios, ECHO, STDIN_FILENO, TCSAFLUSH};
+use libc::{atexit, tcgetattr, tcsetattr, termios, ECHO, STDIN_FILENO, TCSAFLUSH};
+
+static mut ORIG_TERMIOS: termios = unsafe { mem::zeroed() };
+
+extern "C" fn disable_raw_mode() {
+    unsafe {
+        tcsetattr(STDIN_FILENO, TCSAFLUSH, &ORIG_TERMIOS);
+    }
+}
 
 fn enable_raw_mode() {
     unsafe {
-        let mut raw: termios = mem::zeroed();
-
-        tcgetattr(STDIN_FILENO, &mut raw);
+        tcgetattr(STDIN_FILENO, &mut ORIG_TERMIOS);
+        atexit(disable_raw_mode);
+        let mut raw = ORIG_TERMIOS.clone();
         raw.c_lflag &= !ECHO;
         tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
     }
